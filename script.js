34a0a5953db1d64deca052f5cc52d55f7f18e5d5
[Sandeep] <sandeep977244@gmail.com>
2026-05-21 16:32:04 +0530

    Add modal for task creation and enhance drag-and-drop functionality

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const tasks = document.querySelectorAll(".task");
 tasks.forEach((task) => {
   task.addEventListener("drag", (e) => {
     dragElement = task;
-    
   });
 });
 
@@ -15,8 +14,7 @@ function addDragEventsOnColumns(column) {
   column.addEventListener("dragenter", (e) => {
     e.preventDefault();
     column.classList.add("hover-over");
-
-  })
+  });
   column.addEventListener("dragleave", (e) => {
     e.preventDefault();
     column.classList.remove("hover-over");
@@ -27,9 +25,7 @@ function addDragEventsOnColumns(column) {
   });
 
   column.addEventListener("drop", (e) => {
-    
     e.preventDefault();
-    
 
     column.appendChild(dragElement);
     column.classList.remove("hover-over");
@@ -37,5 +33,42 @@ function addDragEventsOnColumns(column) {
 }
 addDragEventsOnColumns(todo);
 addDragEventsOnColumns(progress);
-addDragEventsOnColumns(done);   
+addDragEventsOnColumns(done);
 
+// Modal related code
+const toggleModalBtn = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
+const modalBG = document.querySelector(".modal .bg");
+const addTaskButton=document.querySelector("#add-new-task");
+
+toggleModalBtn.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
+
+modalBG.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+addTaskButton.addEventListener("click",()=>{
+
+  const taskTitleInput=document.querySelector("#task-title-input").value;
+  const taskDescriptionInput=document.querySelector("#task-description-input").value;
+
+const div=document.querySelector("div");
+
+div.classList.add("task");
+  div.setAttribute("draggable","true");
+
+  div.innerHTML=`
+  <h2>${taskTitleInput}</h2>
+  <p>${taskDescriptionInput}</p>
+  <button>Delete</button>
+  `
+  todo.appendChild(div);
+  modal.classList.remove("active");
+    
+  div.addEventListener("drag", (e) => {
+    dragElement = div;
+  });
+
+});
